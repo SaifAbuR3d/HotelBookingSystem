d846d9a6eb9f716447dd46e968121c89e8a08d4a
SaifAbuR3d <saefraed2@gmail.com>
2023-12-19 21:25:56 +0200

    Add RoomController

---
+++ HotelBookingSystem.Api/Controllers/RoomsController.cs
@@ -0,0 +1,98 @@
+﻿using HotelBookingSystem.Application.DTOs.Room;
+using HotelBookingSystem.Application.ServiceInterfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelBookingSystem.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class RoomsController(IRoomService roomService) : ControllerBase
+{
+    /// <summary>
+    /// Get all rooms
+    /// </summary>
+    /// <returns>All rooms</returns>
+    /// <response code="200">Returns all rooms</response>
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<RoomOutputModel>>> GetAllRooms()
+    {
+        var rooms = await roomService.GetAllRoomsAsync();
+        return Ok(rooms);
+    }
+
+    /// <summary>
+    /// Get a room by its id
+    /// </summary>
+    /// <param name="id">The id of the room</param>
+    /// <returns>The room with the given id</returns>
+    /// <response code="200">Returns the room with the given id</response>
+    /// <response code="404">If the room is not found</response>
+    [HttpGet("{id}")]
+    public async Task<ActionResult<RoomOutputModel>> GetRoom(Guid id)
+    {
+        var room = await roomService.GetRoomAsync(id);
+        return Ok(room);
+    }
+
+    /// <summary>
+    /// Create a new room
+    /// </summary>
+    /// <param name="request">The data for the new room</param>
+    /// <returns>The newly created room</returns>
+    /// <response code="201">Returns the newly created room</response>
+    /// <response code="400">If the request data is invalid</response>
+    [HttpPost]
+    public async Task<ActionResult<RoomOutputModel>> CreateRoom(CreateRoomCommand request)
+    {
+        var room = await roomService.CreateRoomAsync(request);
+
+        if (room is null)
+        {
+            return BadRequest();
+        }
+
+        return CreatedAtAction(nameof(GetRoom), new { id = room.Id }, room);
+    }
+
+    /// <summary>
+    /// Delete a room
+    /// </summary>
+    /// <param name="id">The id of the room to delete</param>
+    /// <returns>No content</returns>
+    /// <response code="204">If the room is successfully deleted</response>
+    /// <response code="404">If the room is not found</response>
+    [HttpDelete("{id}")]
+    public async Task<ActionResult> DeleteRoom(Guid id)
+    {
+        var deleted = await roomService.DeleteRoomAsync(id);
+
+        if (!deleted)
+        {
+            return NotFound();
+        }
+
+        return NoContent();
+    }
+
+    /// <summary>
+    /// Update a room
+    /// </summary>
+    /// <param name="id">The id of the room to update</param>
+    /// <param name="request">The new data for the room</param>
+    /// <returns>No content</returns>
+    /// <response code="204">If the room is successfully updated</response>
+    /// <response code="404">If the room is not found</response>
+    /// <response code="400">If the request data is invalid</response>
+    [HttpPut("{id}")]
+    public async Task<ActionResult<RoomOutputModel>> UpdateRoom(Guid id, UpdateRoomCommand request)
+    {
+        var updated = await roomService.UpdateRoomAsync(id, request);
+
+        if (!updated)
+        {
+            return NotFound();
+        }
+
+        return NoContent();
+    }
+}
