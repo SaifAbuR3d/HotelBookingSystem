adf14448c60caa3c2f51ce886e266f56f2fd7144
SaifAbuR3d <saefraed2@gmail.com>
2024-01-08 16:15:29 +0200

    Implement CurrentUser as an abstraction above IHttpContextAccessor to get information(claims) about the current authenticated user

---
+++ HotelBookingSystem.Api/Services/CurrentUser.cs
@@ -0,0 +1,35 @@
+﻿using HotelBookingSystem.Application.Abstractions;
+using HotelBookingSystem.Application.Exceptions;
+using HotelBookingSystem.Application.Identity;
+using System.Security.Claims;
+
+namespace HotelBookingSystem.Api.Services;
+
+/// <summary>
+/// Get information about the current authenticated user
+/// </summary>
+/// <param name="httpContextAccessor"></param>
+public class CurrentUser(IHttpContextAccessor httpContextAccessor) : ICurrentUser
+{
+    /// <summary>
+    /// Get the Id of the current authenticated user
+    /// </summary>
+    public string Id => httpContextAccessor.HttpContext?.User.FindFirstValue(ClaimTypes.NameIdentifier)
+            ?? throw new UnauthenticatedException();
+
+    /// <summary>
+    /// Get the role of the current authenticated user
+    /// </summary>
+    public string Role => httpContextAccessor.HttpContext?.User.FindFirstValue(ClaimTypes.Role)
+            ?? throw new UnauthenticatedException();
+
+    /// <summary>
+    /// returns true is the current authenticated user is a guest
+    /// </summary>
+    public bool IsGuest => Role == UserRoles.Guest;
+
+    /// <summary>
+    /// returns true is the current authenticated user is an admin
+    /// </summary>
+    public bool IsAdmin => Role == UserRoles.Admin;
+}
---
+++ HotelBookingSystem.Application/Abstractions/ICurrentUser.cs
@@ -0,0 +1,9 @@
+﻿namespace HotelBookingSystem.Application.Abstractions;
+
+public interface ICurrentUser
+{
+    string Id { get; }
+    string Role { get; }
+    bool IsGuest { get; }
+    bool IsAdmin { get; }
+}
