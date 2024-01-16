42cfa94a47b8180305c46436d1f0318fa83becdc
SaifAbuR3d <saefraed2@gmail.com>
2024-01-16 04:28:24 +0200

    Add Pdf infrastructure and needed packages for email and comments .xml Application doc file

---
+++ HotelBookingSystem.Infrastructure/PDF/PdfInfrastructureConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using HotelBookingSystem.Application.Abstractions.InfrastructureInterfaces.PdfInterfaces;
+using Microsoft.Extensions.DependencyInjection;
+using QuestPDF.Infrastructure;
+
+namespace HotelBookingSystem.Infrastructure.PDF;
+
+public static class PdfInfrastructureConfiguration
+{
+    public static IServiceCollection AddPdfInfrastructure(
+        this IServiceCollection services)
+    {
+        QuestPDF.Settings.License = LicenseType.Community;
+        return services.AddScoped<IPdfGenerator, PdfGenerator>();
+    }
+}
